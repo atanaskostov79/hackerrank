9a807b4af1cab1bc0ee9ffd529e5cfd78846fa50
Atanas Kostov <ankostov@gmail.com>
2023-02-19 12:17:56 +0200

    main

---
+++ Algorithms/migratory-birds.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+os.environ['OUTPUT_PATH'] = "junk.txt"
+def migratoryBirds(arr):
+    freq = Counter(arr)
+    return max(set(freq), key=arr.count)
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
